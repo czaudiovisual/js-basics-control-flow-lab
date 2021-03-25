f55f1f922645dc2720abcbc77622ae31e4b7978b
Christian Bermeo <czaudiovisual@hotmail.com>
2021-03-25 00:55:24 -0400

    Completed assignment

--- index.js
+++ index.js
@@ -1,11 +1,30 @@
-function scuberGreetingForFeet(){
+function scuberGreetingForFeet(distance){
   // Write your code here!
+  if (distance <= 400) {
+    return 'This one is on me!';
+  } else if (distance > 2500) {
+    return 'No can do.';
+  } else if (distance > 2000)
+    return 'I will gladly take your thirty bucks.';
 }
 
-function ternaryCheckCity(){
+function ternaryCheckCity(city){
   // Write your code here!
+  return city === 'NYC' ? 'Ok, sounds good.' : 'No go.';
 }
 
-function switchOnCharmFromTip(){
+function switchOnCharmFromTip(tip){
   // Write your code here!
+  let goodbyeExpression;
+  switch (true) {
+    case tip === 'generous':
+    goodbyeExpression = 'Thank you so much.';
+    break;
+    case tip === 'not as generous':
+      goodbyeExpression = 'Thank you.';
+    break;
+    default : 
+    goodbyeExpression = 'Bye.';
+  }
+return goodbyeExpression;
 }
